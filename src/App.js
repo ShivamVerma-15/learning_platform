39d524dbc17aa657535ec4d753a85e7da43e7d00
ShivamVerma-15 <shivam_verma@persistent.co.in>
2021-04-24 16:12:29 +0530

    Completed the feedback component functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Body from './components/course_enrollment_component/body';
+import Feedback from './components/course_enrollment_component/Feedback';
 
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
-      <Body />
+      {/* <Body /> */}
+      <Feedback/>
       {/* <Footer /> */}
     </BrowserRouter >
   );
